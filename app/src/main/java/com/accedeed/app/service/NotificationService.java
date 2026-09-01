74606e7fed3884a7956b6d0649d6a18847b6435e
sebass <sebastianlgr10@gmail.com>
2026-08-31 19:39:14 -0600

    Add settings.gradle and missing resources (strings, colors, themes, drawables)

---
+++ app/src/main/java/com/accedeed/app/service/NotificationService.java
@@ -0,0 +1,18 @@
+package com.accedeed.app.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class NotificationService extends Service {
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+}
